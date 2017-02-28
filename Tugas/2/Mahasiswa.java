35127f63b0d617227ff11c1ef00300baaa50bda3
canuesp <canuedra@gmail.com>
2017-02-28 16:55:36 +0700

    Add files via upload

---
+++ Tugas/2/Mahasiswa.java
@@ -0,0 +1,8 @@
+public class Mahasiswa
+{public static void main(String[] args)
+	{
+		System.out.println("Nama: Canu Edra Sugeng Putra");
+		System.out.println("Nim: 10114257");
+		System.out.println("Alamat: Jalan Anoa X no 153");
+	}
+}
